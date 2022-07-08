9cbc206e66e6c66f4d1ad1fc87d9164f2d630808
shen0512 <shen0512@github.com>
2022-07-08 22:42:57 +0800

    tmp

--- AVFoundationWithRecorder/ViewController.h
+++ AVFoundationWithRecorder/ViewController.h
@@ -6,9 +6,17 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<AVCaptureVideoDataOutputSampleBufferDelegate>
 
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (nonatomic, strong) AVCaptureSession *captureSession;
+@property (strong, nonatomic) AVAssetWriter *assetWriter;
+@property (strong, nonatomic) AVAssetWriterInput *assertWriterInput;
+
+@property int recordStatus; //0->no, 1->init ,2->start record
+@property (strong, nonatomic) NSString *videoOutPath;
 
 @end
 
--- AVFoundationWithRecorder/ViewController.m
+++ AVFoundationWithRecorder/ViewController.m
@@ -15,8 +15,118 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    [self setupCamera];
+    [_captureSession startRunning];
+    
+    self.recordStatus = 0;
 }
 
+- (void)setupCamera{
+    // Do any additional setup after loading the view.
+    AVCaptureDevice *captureDevice = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    // Get an instance of the AVCaptureDeviceInput class using the previous device object.
+    AVCaptureDeviceInput *input = [AVCaptureDeviceInput deviceInputWithDevice:captureDevice error:nil];
+    
+    
+    // Initialize the captureSession object.
+    _captureSession = [[AVCaptureSession alloc] init];
+    // Set the input device on the capture session.
+    [_captureSession addInput:input];
+    [_captureSession setSessionPreset:AVCaptureSessionPreset1920x1080];
+    
+    // Initialize a AVCaptureMetadataOutput object and set it as the output device to the capture session.
+    AVCaptureVideoDataOutput *captureVideoDataOutput = [[AVCaptureVideoDataOutput alloc] init];
+    [_captureSession addOutput:captureVideoDataOutput];
+    
+    AVCaptureConnection *connection = [captureVideoDataOutput connectionWithMediaType:AVMediaTypeVideo];
+    connection.videoOrientation = AVCaptureVideoOrientationPortrait;
+    [connection setPreferredVideoStabilizationMode:AVCaptureVideoStabilizationModeStandard];
+    
+    // Create a new serial dispatch queue.
+    [captureVideoDataOutput setSampleBufferDelegate:self queue:dispatch_queue_create("myQueue", NULL)];
+}
+
+- (void)setupRecorder{
+    NSError *error = nil;
+    NSArray *pathDocuments = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *outputURL = pathDocuments[0];
+    
+    self.videoOutPath = [[outputURL stringByAppendingPathComponent:[NSString stringWithFormat:@"%u", arc4random() % 1000]] stringByAppendingPathExtension:@"mp4"];
+    self.assetWriter = [AVAssetWriter assetWriterWithURL:[NSURL fileURLWithPath:self.videoOutPath] fileType:AVFileTypeMPEG4 error:&error];
+    
+    NSNumber* width= @1080;
+    NSNumber* height = @1920;
+    
+    NSDictionary *compressionProperties = @{AVVideoProfileLevelKey         : AVVideoProfileLevelH264HighAutoLevel,
+                                            AVVideoH264EntropyModeKey      : AVVideoH264EntropyModeCABAC,
+                                            AVVideoAverageBitRateKey       : @([width intValue]* [height intValue]* 11.4)};
+    
+    
+    NSDictionary *videoSettings = @{AVVideoCompressionPropertiesKey : compressionProperties,
+                                    AVVideoCodecKey                 : AVVideoCodecTypeH264,
+                                    AVVideoWidthKey                 : width,
+                                    AVVideoHeightKey                : height};
+
+    self.assertWriterInput = [AVAssetWriterInput assetWriterInputWithMediaType:AVMediaTypeVideo outputSettings:videoSettings];
+
+    if([self.assetWriter canAddInput:self.assertWriterInput]){
+        [self.assetWriter addInput:self.assertWriterInput];
+        [self.assertWriterInput setExpectsMediaDataInRealTime:YES];
+    }
+    
+}
+
+- (void)captureOutput:(AVCaptureOutput *)output didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer fromConnection:(AVCaptureConnection *)connection{
+    
+    dispatch_sync(dispatch_get_main_queue(), ^(){
+        CVPixelBufferRef pixelBuffer = (CVPixelBufferRef)CMSampleBufferGetImageBuffer(sampleBuffer);
+        CIImage *ciImage = [CIImage imageWithCVPixelBuffer:pixelBuffer];
+
+        CIContext *temporaryContext = [CIContext contextWithOptions:nil];
+        CGImageRef videoImage = [temporaryContext createCGImage:ciImage
+                                                       fromRect:CGRectMake(0, 0, CVPixelBufferGetWidth(pixelBuffer), CVPixelBufferGetHeight(pixelBuffer))];
+            
+        UIImage *uiImage = [UIImage imageWithCGImage:videoImage];
+        CGImageRelease(videoImage);
+        
+        _imageView.image = uiImage;
+        
+        if(self.recordStatus == 1){
+            [self.assetWriter startWriting];
+            [self.assetWriter startSessionAtSourceTime:CMSampleBufferGetPresentationTimeStamp(sampleBuffer)];
+            self.recordStatus = 2;
+        }else if(self.recordStatus == 2 && self.assertWriterInput.isReadyForMoreMediaData){
+            [self.assertWriterInput appendSampleBuffer:sampleBuffer];
+        }
+        
+    });
+}
+- (IBAction)recordClick:(id)sender {
+    UIButton *button = (UIButton*)sender;
+    
+    if(self.recordStatus == 0){
+        NSLog(@"start record");
+        
+        [self setupRecorder];
+        self.recordStatus = 1;
+        [button setTitle:@"Stop" forState:UIControlStateNormal];
+    }else{
+        NSLog(@"end record");
+        
+        self.recordStatus = 0;
+        
+        [self.assertWriterInput markAsFinished];
+        [self.assetWriter finishWritingWithCompletionHandler:^{
+            NSLog(@"video path: %@", self.videoOutPath);
+            
+            self.assertWriterInput = nil;
+            self.assetWriter = nil;
+            self.videoOutPath = nil;
+        }];
+        
+        [button setTitle:@"Record" forState:UIControlStateNormal];
+    }
+}
 
 @end
